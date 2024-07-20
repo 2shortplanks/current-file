c1ac704611ae78d4124ea13f33597610eb9e043a
Mark Fowler <mark@twoshortplanks.com>
2024-07-20 06:13:22 -0400

    Working with settings

--- main.js
+++ main.js
@@ -3,8 +3,14 @@ const path = require("path");
 const os = require("os");
 const obsidian = require("obsidian");
 
+const DEFAULT_SETTINGS = {
+  path: ".obsidian-current-file.json",
+};
+
 module.exports = class CurrentFilePlugin extends obsidian.Plugin {
-  onload() {
+  async onload() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+
     if (!this.app.isMobile) {
       // navigation
       this.registerEvent(
@@ -23,6 +29,8 @@ module.exports = class CurrentFilePlugin extends obsidian.Plugin {
       this.registerEvent(
         this.app.vault.on("rename", this.writeCurrentFilename.bind(this))
       );
+
+      this.addSettingTab(new CurrentFileSettingTab(this.app, this));
     }
   }
 
@@ -55,10 +63,7 @@ module.exports = class CurrentFilePlugin extends obsidian.Plugin {
     // write the file out
     // by using resolve instead of join, we can handle the case where the
     // filename is a filename is an absolute path
-    const destFilename = path.resolve(
-      os.homedir(),
-      ".obsidian-current-file.json"
-    );
+    const destFilename = path.resolve(os.homedir(), this.settings.path);
     fs.writeFile(destFilename, encodedJSON, (err) => {
       if (err) {
         console.error("Error writing filename to file", err);
@@ -66,3 +71,33 @@ module.exports = class CurrentFilePlugin extends obsidian.Plugin {
     });
   }
 };
+
+// Settings tab
+class CurrentFileSettingTab extends obsidian.PluginSettingTab {
+  constructor(app, plugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display() {
+    const { containerEl } = this;
+
+    containerEl.empty();
+
+    containerEl.createEl("h2", { text: "Current File Plugin Settings" });
+
+    new obsidian.Setting(containerEl)
+      .setName("Path")
+      .setDesc(
+        "Where to save the current file information. " +
+          "If this is not an absolute path then it will be treated as " +
+          "relative to your home directory / profile directory."
+      )
+      .addText((text) =>
+        text.setValue(this.plugin.settings.path).onChange(async (value) => {
+          this.plugin.settings.path = value;
+          await this.plugin.saveData(this.plugin.settings);
+        })
+      );
+  }
+}
